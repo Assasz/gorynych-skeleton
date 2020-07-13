bc1bd6ec7aeb884309e3490a22b476d1cebca535
Assasz <pawel.assasz678@gmail.com>
2020-07-13 22:22:34 +0200

    Add API functional tests

---
+++ tests/Functional/Api/Client/GetTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Api\Client;
+
+use App\Domain\Entity\Client;
+use Gorynych\Testing\ApiTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class GetTest extends ApiTestCase
+{
+    private const ENDPOINT_URI = '/clients/1';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->entityManager->loadFixtures(['clients.yaml']);
+    }
+
+    public function testStructure(): void
+    {
+        $response = $this->client->request('GET', self::ENDPOINT_URI);
+
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertMatchesItemJsonSchema($response, Client::class);
+    }
+}
